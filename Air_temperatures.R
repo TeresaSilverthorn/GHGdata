0de780c4fd4773946e82128a065ae20e25d00a4c
Teresa Silverthorn <teresa.silverthorn@gmail.com>
2022-04-21 12:02:03 +0200

    Successfully filled the gaps in HOBO data and made loop for HOBO data 2022-04-21

--- Air_temperatures.R
+++ Air_temperatures.R
@@ -7,6 +7,8 @@ library(dplyr)
 library(lubridate)
 library(ggplot2)
 library(survival)
+library(scales)
+library(tidyr)
 #
 ### Goal:  Extract air temperature data from the ibutton dataloggers as well as Naiara's HOBO dataloggers to get an average air temperature per GHG measurement to correct for in the gasflux calculation ###
 #
@@ -161,14 +163,14 @@ write.table(matTemp,file="C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Do
 #Will need to re download all or just the problem files from the HOBO software. 
 
 
-testAL01<-do.call(rbind, lapply(list.files("C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Data/Ancillary Data/light loggers_FRA/By site/AL01", pattern='csv', full.names=T,  recursive=TRUE), fread, select = c(2:4), skip=2, header=F, col.names=c("Datetime", "Temp_C", "Light_intensity"))) #OK the workaround means no header but we can add those later. #For one site/folder #next try loop
+#testAL01<-do.call(rbind, lapply(list.files("C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Data/Ancillary Data/light loggers_FRA/By site/AL01", pattern='csv', full.names=T,  recursive=TRUE), fread, select = c(2:4), skip=2, header=F, col.names=c("Datetime", "Temp_C", "Light_intensity"))) #OK the workaround means no header but we can add those later. #For one site/folder #next try loop
 
 
-testAL01$Datetime <- as.POSIXct(testAL01$Datetime, format = "%m/%d/%y %I:%M:%S %p", tz="Europe/Paris", origin = "1970-01-01")
+#testAL01$Datetime <- as.POSIXct(testAL01$Datetime, format = "%m/%d/%y %I:%M:%S %p", tz="Europe/Paris", origin = "1970-01-01")
 
-plot <- ggplot(testAL01, aes(x=Datetime, y=Temp_C)) +
-  geom_line()  
-plot
+#plot <- ggplot(testAL01, aes(x=Datetime, y=Temp_C)) +
+  #geom_line()  
+#plot
 
 #VI01<-do.call(rbind, lapply(list.files("C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Data/Ancillary Data/light loggers_FRA/By site/VI01", pattern='csv', full.names=T,  recursive=TRUE), fread, select = c(2:4), skip=2, header=F, col.names=c("Datetime", "Temp_C", "Light_intensity"))) 
 
@@ -184,8 +186,8 @@ filelist <- list.files(path="C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)
 
 myplot <- function(data, title){
   ggplot(data, aes(x = Datetime,y=Temp_C)) +
-    geom_line () +
-    labs(title = title)
+    geom_line () + scale_x_datetime(breaks=date_breaks("1 month"), date_labels = "%b") + 
+    labs(title = title) # function to make plots in loop
 }
   
 
@@ -206,30 +208,186 @@ setwd("C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Data/Ancill
 
 write.csv(Dat, paste0("HOBO_temp_", i, ".csv"))
 
-#print(myplot(get(i), i))#try to make a loop to plot each site to see if we have any gaps
+print(myplot(get(i), i)) #loop to plot each site to see if we have any gaps
+#
 #RA01 stops in September
 #ME01 gap September to November
-#GR02 gap September to November
+#GR01 gap September to November
 #AL03 gap July to September
 #AL02 only Dec to Feb #No problem, totally replaced with AL01 in next loop
 
 }
 
+#Next step is to fill the gaps with temperature data from Romain's ibuttons or weather station data
 
+#Identify the gaps:
+#AL01: April - Feb
+#AL02: Dec - Feb            * Replace completely with AL01, except for Dec-Feb if possible *
+#AL03: Jun - Jul, Sep (2021-09-14 15:19:02) - Dec (2021-11-26 13:14:03)  overlaps with all campaign OK
+#AL04: April - Dec
+#AL05 April - April
+#AL06: April - Feb
+#AL07: April - Dec
+#BR01: Jul - Feb            no problems, covers the June campaign OK
+#BR02: April - Feb
+#BU01: April - Oct          * ends 2021-10-25 10:21:01 thus doesn't cover November campaign *
+#BU02: April - Feb
+#CA01: April - Feb
+#CA02: April - Feb
+#CO01: April - Feb
+#GR01: Jun - Sep (2021-09-13 14:02:46), Nov (2021-11-22 08:00:00) - Feb * therefore missing the October campaign *
+#JO01: Jun - Feb            
+#ME01: May-Sep (2021-09-17 15:17:09), Nov (2021-10-28 10:50:54)-Feb     * therefore missing the October campaign *
+#RA01: April - Sep  (2021-09-15 16:41:40)     * therefore missing the October and November campaign  *        
+#RO01: April - Oct (2021-10-27 16:20:30)         * therefore missing the November campaign *
+#VI01: April - Feb  
+
+## Read-in Romain's leaf pack ibutton metadata:
+
+Romain_dat <- read.csv("C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Data/Ancillary Data/iButton_data_Dryver/Romain_ibutton_temperatures.csv", sep=";")
+
+#Now check if he has the data for: 
+# BU01 November campaign 72_B200000031DB6D21_010422
+BU01_11.2021 <- fread("C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Data/Ancillary Data/iButton_data_Dryver/October 2021_leafpacks/72_B200000031DB6D21_010422.csv")
+colnames(BU01_11.2021) <- c("Datetime", "Unit", "Temp_C")
+BU01_11.2021$Datetime <- as.POSIXct(BU01_11.2021$Datetime, format = "%d/%m/%y %I:%M:%S %p", tz="Europe/Paris", origin = "1970-01-01")
+
+BU01plot<- ggplot(BU01_11.2021, aes(x = Datetime,y=Temp_C)) +
+  geom_line () + scale_x_datetime(breaks=date_breaks("1 month"), date_labels = "%b") 
+BU01plot
+
+# GR01 October campaign 62_F900000031DAFC21_010422
+GR01_10.2021 <- fread("C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Data/Ancillary Data/iButton_data_Dryver/October 2021_leafpacks/62_F900000031DAFC21_010422.csv")
+colnames(GR01_10.2021) <- c("Datetime", "Unit", "Temp_C")
+GR01_10.2021$Datetime <- as.POSIXct(GR01_10.2021$Datetime, format = "%d/%m/%y %I:%M:%S %p", tz="Europe/Paris", origin = "1970-01-01") #GHG measures on October 26th at 15:30 
+
+GR01plot<- ggplot(GR01_10.2021, aes(x = Datetime,y=Temp_C)) +
+  geom_line () + scale_x_datetime(breaks=date_breaks("1 week"), date_labels = "%d/%m") 
+GR01plot
+
+
+# ME01 October campaign 63_0D00000031F9BD21_010422
+ME01_10.2021 <- fread("C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Data/Ancillary Data/iButton_data_Dryver/October 2021_leafpacks/63_0D00000031F9BD21_010422.csv")
+colnames(ME01_10.2021) <- c("Datetime", "Unit", "Temp_C")
+ME01_10.2021$Datetime <- as.POSIXct(ME01_10.2021$Datetime, format = "%d/%m/%y %I:%M:%S %p", tz="Europe/Paris", origin = "1970-01-01") #GHG measures Oct 28 at 10:30
+
+ME01plot<- ggplot(ME01_10.2021, aes(x = Datetime,y=Temp_C)) +
+  geom_line () + scale_x_datetime(breaks=date_breaks("1 week"), date_labels = "%d/%m") 
+ME01plot
+
+# RA01 October and November campaign 81_4200000031FB2121_010422
+RA01_10.2021 <- fread("C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Data/Ancillary Data/iButton_data_Dryver/October 2021_leafpacks/81_4200000031FB2121_010422.csv")
+colnames(RA01_10.2021) <- c("Datetime", "Unit", "Temp_C")
+RA01_10.2021$Datetime <- as.POSIXct(RA01_10.2021$Datetime, format = "%d/%m/%y %I:%M:%S %p", tz="Europe/Paris", origin = "1970-01-01") #GHG measures at Oct 25th at 12:00
+
+RA01plot<- ggplot(RA01_10.2021, aes(x = Datetime,y=Temp_C)) +
+  geom_line () + scale_x_datetime(breaks=date_breaks("1 week"), date_labels = "%d/%m") 
+RA01plot
+
+
+# RO01 November campaign 95_2700000031E3B821_010422
+RO01_11.2021 <- fread("C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Data/Ancillary Data/iButton_data_Dryver/October 2021_leafpacks/95_2700000031E3B821_010422.csv")
+colnames(RO01_11.2021) <- c("Datetime", "Unit", "Temp_C")
+RO01_11.2021$Datetime <- as.POSIXct(RO01_11.2021$Datetime, format = "%d/%m/%y %I:%M:%S %p", tz="Europe/Paris", origin = "1970-01-01")
+
+RO01plot<- ggplot(RO01_11.2021, aes(x = Datetime,y=Temp_C)) +
+  geom_line () + scale_x_datetime(breaks=date_breaks("1 week"), date_labels = "%d/%m") 
+RO01plot
+
+
+#################################################################
+#Now  for each case, bind data to cover the holes
+
+#BU01
+#Remove rows with NA's, clip Romain's data so that it doesn't over lap
+BU01 <- BU01 %>% drop_na()
+BU01_11.2021 <- subset(BU01_11.2021, Datetime > ("2021-10-25 10:12:48") ) #1831-1711= 120 (dropped 120 obs)
+str(BU01) #27237 obs. of  3 variables
+BU01 <- rbind(BU01, #2021-04-19 07:00:00 - 2021-10-25 10:12:48 
+              BU01_11.2021, fill=TRUE) # 	2021-10-20 10:51:00 - 2022-01-04 15:51:00 #fill=TRUE sets NAs for non-matching columns
+str(BU01) #28948 obs. of  4
+#Must remove the 4th column (Unit)
+BU01 <- subset(BU01, select = -c(Unit) )
+plot1<- ggplot(BU01, aes(x = Datetime,y=Temp_C)) +
+  geom_line () + scale_x_datetime(breaks=date_breaks("1 month"), date_labels = "%b") 
+plot1
+write.csv(BU01, "C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Data/Ancillary Data/light loggers_FRA/Filled air temp files/HOBO_temp_BU01.csv")
+
+######################
+#GR01
+#Remove rows with NA's, clip Romain's data so that it doesn't over lap
+GR01 <- GR01 %>% drop_na()
+GR01_10.2021 <- subset(GR01_10.2021, Datetime > ("2021-09-13 14:02:46") ) 
+GR01_10.2021 <- subset(GR01_10.2021, Datetime < ("2021-11-22 08:00:006") ) 
+str(GR01) #23560 obs. of  3 variables
+GR01 <- rbind(GR01, # gap 2021-09-13 14:02:46 -2021-11-22 08:00:00
+              GR01_10.2021, fill=TRUE) # 2021-10-20 10:05:00 - 2022-01-04 15:05:00
+str(GR01) #24351 obs. of  4 variables
+GR01 <- subset(GR01, select = -c(Unit) )
+plot2<- ggplot(GR01, aes(x = Datetime,y=Temp_C)) +
+  geom_line () + scale_x_datetime(breaks=date_breaks("1 month"), date_labels = "%b") 
+plot2
+write.csv(GR01, "C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Data/Ancillary Data/light loggers_FRA/Filled air temp files/HOBO_temp_GR01.csv")
+
+######################
+# ME01 
+ME01 <- ME01 %>% drop_na()
+ME01_10.2021 <- subset(ME01_10.2021, Datetime > ("2021-09-17 15:17:09") ) 
+ME01_10.2021 <- subset(ME01_10.2021, Datetime < ("2021-10-28 10:50:54 ") ) 
+str(ME01) #36670 obs. of  3 variables:
+ME01 <- rbind(ME01, # gap: 2021-09-17 15:17:09 to 2021-10-28 10:50:54   
+              ME01_10.2021, fill=TRUE) # 
+str(ME01) #36862 obs. of  4 variables
+ME01 <- subset(ME01, select = -c(Unit) )
+plot3<- ggplot(ME01, aes(x = Datetime,y=Temp_C)) +
+  geom_line () + scale_x_datetime(breaks=date_breaks("1 month"), date_labels = "%b") 
+plot3
+write.csv(ME01, "C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Data/Ancillary Data/light loggers_FRA/Filled air temp files/HOBO_temp_ME01.csv")
+
+######################
+# RA01
+RA01 <- RA01 %>% drop_na()
+#subset RA01 to remove the 0 in late November
+RA01 <- subset(RA01, Datetime < ("2021-09-15 16:41:40") )
+RA01_10.2021 <- subset(RA01_10.2021, Datetime > ("2021-09-15 16:41:40") ) #doesn't remove any
+str(RA01) #21514 obs. of  3 variables 
+RA01 <- rbind(RA01, # stops 2021-09-15 16:41:40  
+              RA01_10.2021, fill=TRUE) # 
+str(RA01) #23344 obs. of  4 variables
+RA01 <- subset(RA01, select = -c(Unit) )
+plot4<- ggplot(RA01, aes(x = Datetime,y=Temp_C)) +
+  geom_line () + scale_x_datetime(breaks=date_breaks("1 month"), date_labels = "%b") 
+plot4
+write.csv(RA01, "C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Data/Ancillary Data/light loggers_FRA/Filled air temp files/HOBO_temp_RA01.csv")
+
+######################
+# RO01
+RO01 <- RO01 %>% drop_na()
+RO01_11.2021 <- subset(RO01_11.2021, Datetime > ("2021-10-27 16:20:30") )
+str(RO01) #27568 obs. of  3 variables
+RO01 <- rbind(RO01, # Stops 2021-10-27 16:20:30
+              RO01_11.2021, fill=TRUE) # 
+str(RO01) #29224 obs. of  4 variables
+RO01 <- subset(RO01, select = -c(Unit) )
+plot5<- ggplot(RO01, aes(x = Datetime,y=Temp_C)) +
+  geom_line () + scale_x_datetime(breaks=date_breaks("1 month"), date_labels = "%b") 
+plot5
+write.csv(RO01, "C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Data/Ancillary Data/light loggers_FRA/Filled air temp files/HOBO_temp_RO01.csv")
 
 ################################################################################
-## Now run the same loop for the HOBOs 
+########## Now run the same loop to select the nearest temperature for the HOBO data ######################
+## In the "Filled air temp files" folder you will need to add all of the other files that did not need to be filled, and make this the folder locatio for the loop
 
 air_temp_HOBO <- subset(air_temp2, date > as.Date("2021-06-20") ) #Will need to subset the data to exclude the first two campaigns
 
 matTemp2<-matrix(rep(NA,nrow(air_temp_HOBO)*1),nrow=nrow(air_temp_HOBO),ncol=1,dimnames=(list(air_temp_HOBO$ID_unique ,c("Temp")))) #creates a matrix with one column for the temperature associated with each unique IF
 
-## Loop for finding nearest temperature to each start time (ibuttons) 
+## Loop for finding nearest temperature to each start time (HOBOs) 
 
 for(i in 1:nrow(air_temp_HOBO)){
   I<-air_temp_HOBO[i,]
   ID<-I$File_name
-  Dat <- fread(paste("C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Data/Ancillary Data/light loggers_FRA/New HOBO files/",ID,sep=""), header=TRUE)
+  Dat <- fread(paste("C:/Users/teresa.silverthorn/Dropbox/My PC (lyp5183)/Documents/Data/Ancillary Data/light loggers_FRA/Filled air temp files/",ID,sep=""), header=TRUE)
   colnames(Dat) <- c("", "Datetime", "Temp_C", "Light_intensity")
   Dat$Datetime <- as.POSIXct(Dat$Datetime, format = "%Y/%m/%d %I:%M:%S %p", tz="Europe/Paris", origin = "1970-01-01")
   
